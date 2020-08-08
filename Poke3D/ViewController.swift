f0b73a9ebfe03e27590724bfcfe77bd330a39723
oranageHasu <blair.petrachek@gmail.com>
2020-08-08 17:19:24 -0500

    Initial Checkin
    Simple proof of concept uses SceneKit to track for 2 specific pokemon cards, rendering a Scene Asset specific to the identified card.

--- Poke3D/ViewController.swift
+++ Poke3D/ViewController.swift
@@ -23,19 +23,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        // Allows SceneKit to automatically add lighting to the scene
+        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        // Create a session configuration looking for a specific image
+        let configuration = ARImageTrackingConfiguration()
 
+        let imagesToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main)
+        
+        if let images = imagesToTrack {
+            configuration.trackingImages = images
+            configuration.maximumNumberOfTrackedImages = 2
+            print("Images Successfully Added.")
+        }
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -48,28 +53,27 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 
     // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+        
+        if let imageAnchor = anchor as? ARImageAnchor {
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height:  imageAnchor.referenceImage.physicalSize.height)
+            plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+            
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -.pi / 2
+            
+            node.addChildNode(planeNode)
+            
+            if let pokeScene = SCNScene(named: "art.scnassets/\(imageAnchor.referenceImage.name!).scn") {
+                if let pokeNode = pokeScene.rootNode.childNodes.first {
+                    // Rotate clockwise 90 degrees on the x-axis
+                    pokeNode.eulerAngles.x = .pi / 2
+                    planeNode.addChildNode(pokeNode)
+                }
+            }
+        }
+        
         return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
